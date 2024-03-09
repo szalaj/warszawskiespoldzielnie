39789de5eda2bddb25434a951b759818cc16b47c
szalaj <mszalajski@gmail.com>
2024-03-09 09:39:03 +0100

    x

--- pkgs/spoldzielnie/spoldzielnie/routes/common.py
+++ pkgs/spoldzielnie/spoldzielnie/routes/common.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, request, send_file, sessions
+import geojson
 common = Blueprint('common', __name__)
 
 @common.route('/', methods=['GET', 'POST'])
 def main():
 
+    # with open('static/dzielnice/warszawa-dzielnice.geojson', 'r') as F:
+    #     city_districts = geojson.loads(F.read())    
 
     return render_template('index.html')
