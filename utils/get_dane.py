f06c721c6132a531b92d170b68424f3857113667
szalaj <mszalajski@gmail.com>
2024-03-29 21:41:07 +0100

    lista spoldzlelni

--- utils/get_dane.py
+++ utils/get_dane.py
@@ -1,14 +1,18 @@
 import requests
 
 # create new csv file to write later
+import pandas as pd
 
 
 with open('../pkgs/spoldzielnie/spoldzielnie/dane/spoldzielnie_krs.txt', 'r') as f:
     data = f.read()
     # get first line
 
+# create new pandas dataframe
+df = pd.DataFrame(columns=['nazwa', 'krs', 'nip', 'regon', 'adres', 'kod_pocztowy', 'miejscowosc', 'forma_prawna', 'data_rejestracji', 'szer', 'dlug'])
+
 # create and open new csv file to write
-with open('../pkgs/spoldzielnie/spoldzielnie/dane/spoldzielnie_krs_dane.csv', 'w') as f:
+with open('../pkgs/spoldzielnie/spoldzielnie/dane/spoldzielnie_krs_dane3.csv', 'w') as f:
     for i,krs in enumerate(data.split('\n')):
         if i !=0:
             print(krs)
@@ -24,7 +28,7 @@ with open('../pkgs/spoldzielnie/spoldzielnie/dane/spoldzielnie_krs_dane.csv', 'w
                 forma_prawna = x.json()['odpis']['dane']['dzial1']['danePodmiotu']['formaPrawna']
                 nip = x.json()['odpis']['dane']['dzial1']['danePodmiotu']['identyfikatory']['nip']
                 regon = x.json()['odpis']['dane']['dzial1']['danePodmiotu']['identyfikatory']['regon']
-                
+
                 if 'ulica' in x.json()['odpis']['dane']['dzial1']['siedzibaIAdres']['adres']:
                     ulica = x.json()['odpis']['dane']['dzial1']['siedzibaIAdres']['adres']['ulica']
                 else:
@@ -43,9 +47,46 @@ with open('../pkgs/spoldzielnie/spoldzielnie/dane/spoldzielnie_krs_dane.csv', 'w
                 kod_pocztowy= x.json()['odpis']['dane']['dzial1']['siedzibaIAdres']['adres']['kodPocztowy']
                 miejscowosc= x.json()['odpis']['dane']['dzial1']['siedzibaIAdres']['adres']['miejscowosc']
 
+                adres_query = f"{ulica} {nr_domu} {miejscowosc}"
+
+                # upper case adres_query
+                adres_query = adres_query.upper()
+
+                # convert adres_query to ascii url format
+
+                adres_query = adres_query.replace('UL.', '')
+                adres_query = adres_query.replace('AL.', '')
+                adres_query = adres_query.replace('FR.', '')
+                adres_query = adres_query.replace('R.MIELCZARSKIEGO', 'MIELCZARSKIEGO')
+                adres_query = adres_query.replace('  ', ' ')
+                adres_query = adres_query.replace(' ', '+')
+                # adres_query='Orlat+Lwowskich+48+Warszawa'
+
+                print(adres_query)
+                if nr_lokalu.strip() != '':
+                    adres = f"{ulica} {nr_domu}, {nr_lokalu}"
+                else:
+                    adres = f"{ulica} {nr_domu}"
+
+                a = requests.get(f"https://nominatim.openstreetmap.org/search?q={adres_query}&format=geojson")
+                if a.status_code == 200 and len(a.json()['features']) > 0:
+                    szer = a.json()['features'][0]['geometry']['coordinates'][1]
+                    dlug = a.json()['features'][0]['geometry']['coordinates'][0]
+                else:
+                    szer = 'n'
+                    dlug = 'n'
+
+                # write data to pandas dataframe
+                df = df._append({'nazwa': nazwa, 'krs': krs, 'nip': nip, 'regon': regon, 'adres': adres, 'kod_pocztowy': kod_pocztowy, 'miejscowosc': miejscowosc, 'forma_prawna': forma_prawna, 'data_rejestracji': data_rejestracji, 'szer': szer, 'dlug': dlug}, ignore_index=True)
+                
                 
                 # write to file
-                f.write(f"{nazwa};{krs};{nip};{regon};{ulica} {nr_domu};{nr_lokalu};{kod_pocztowy};{miejscowosc};;{forma_prawna};{data_rejestracji}\n")
+                f.write(f"{nazwa};{krs};{nip};{regon};{ulica} {nr_domu};{nr_lokalu};{kod_pocztowy};{miejscowosc};;{forma_prawna};{data_rejestracji};{szer};{dlug}\n")
             else:
                 print('error')
                 f.write(f"{krs};error\n")
+
+
+
+# save pandas dataframe to csv file
+df.to_csv('../pkgs/spoldzielnie/spoldzielnie/dane/spoldzielnie_df_4.csv', index=False)
