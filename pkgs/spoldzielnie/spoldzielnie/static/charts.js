e35466049dacc850343b45598fe4f66b60b0cee8
szalaj <mszalajski@gmail.com>
2024-04-10 22:59:53 +0200

    gropings

--- pkgs/spoldzielnie/spoldzielnie/static/charts.js
+++ pkgs/spoldzielnie/spoldzielnie/static/charts.js
@@ -8,6 +8,7 @@ export function wykres_spoldzielni(name) {
         .attr('cy', 50)
         .attr('r', 50)
         .style('fill', 'red');
+
     cz.append('circle')
         .attr('cx', 50)
         .attr('cy', 50)
@@ -17,3 +18,8 @@ export function wykres_spoldzielni(name) {
     return cz.node();
 }
 
+export function daj_mi_wykres(dane) {
+    // const dzielnie = d3.group(dane, (d) => d.dzielnica);
+    const dzielnie = d3.rollup(dane, (D) => d3.sum(D, d => d.bilans), (d) => d.dzielnica);
+    return dzielnie;
+}
