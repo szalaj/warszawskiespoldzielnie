4faf0fa01ae95576487fdab34a5f7b1d963d5fb3
szalaj <mszalajski@gmail.com>
2024-08-10 23:17:48 +0200

    async

--- pkgs/spoldzielnie/src/spoldzielnie/routes/common.py
+++ pkgs/spoldzielnie/src/spoldzielnie/routes/common.py
@@ -18,23 +18,6 @@ def favicon():
 
 @common.route('/', methods=['GET', 'POST'])
 def main():
-    logger.info(f"GET request na głównym widoku")
-    # kr = Spoldzielnia.query.all()
-
-    # S = [i.as_dict() for i in kr]
-    # print(S)
-
-    # UPLOADS_PATH = join(STATIC_FOLDER, 'spoldzielnie.csv')
-
-    # # write S as csv
-    # with open(UPLOADS_PATH ,'w') as f:
-    #     writer = csv.DictWriter(f, fieldnames=S[0].keys())
-    #     writer.writeheader()
-    #     writer.writerows(S)
-
-
-
-
     return render_template('index.html')
 
 
@@ -130,27 +113,5 @@ def walne_dane():
 
 
 
-@common.route('/czujnik', methods=['GET'])
-def czunik():
-    with open(join(STATIC_FOLDER, 'czujnik.txt'), 'a') as f:
-        # current datetime
-        now = datetime.now()
-
-        # convert datetime object to string
-        timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
-
-        f.write(timestamp_str+"\n")
-    print(STATIC_FOLDER)
-    return "czujnik"
 
 
-# create a route which reads the czujnik.txt file and returns the content, keep  new lines
-@common.route('/czujnik_history', methods=['GET'])
-def czujnik_history():
-    with open(join(STATIC_FOLDER, 'czujnik.txt'), 'r') as f:
-        content = f.read()
-        content = content.replace('\n', '<br>')
-        content_array = content.split('<br>')
-        content_array.reverse()
-        content = '<br>'.join(content_array)
-    return content
