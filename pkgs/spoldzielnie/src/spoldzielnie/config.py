9556f3b53b75162fa879d61de52268dac56c4f2a
szalaj <mszalajski@gmail.com>
2024-08-11 14:54:33 +0200

    e

--- pkgs/spoldzielnie/src/spoldzielnie/config.py
+++ pkgs/spoldzielnie/src/spoldzielnie/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/spoldzielnie/static"
     MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/spoldzielnie/media"
-    SECRET_KEY = 'asdfal3l3j4lkjlaksd333'
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + f"{os.getenv('APPDB_PATH')}/dat.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SCHEDULER_API_ENABLED = True
