fe9ed73245272e9f9eac28c91bf49014ab0ccc2f
szalaj <mszalajski@gmail.com>
2024-08-15 22:42:11 +0200

    envs

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import os
 from spoldzielnie import init_app
 from loguru import logger
+import sys
 
 #################################################
 # ustaw zmienne srodowskowe w terminalu przed uruchomieniem aplikacji
 # bazy danych sqlite i
 # sciezek do logow i aplikacji
 ###########################################
+envs_to_set = [
+    'APPDB_PATH',
+    'LOG_PATH',
+    'APP_FOLDER',
+    'FLASK_SECRET_KEY'
+]
+# Check if APPDB_PATH is set
+for env in envs_to_set:
+    if not os.getenv(env):
+        logger.error(f"{env} environment variable is not set")
+        sys.exit(1)
+
 
 
 log_level = "DEBUG"
